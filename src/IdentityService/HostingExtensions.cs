25e1124afabee8d725928aed6e65d725103f3717
ttacompu <ttacompu@gmail.com>
2026-02-23 19:57:21 -0500

    cannot create auction because issuer problem

--- src/IdentityService/HostingExtensions.cs
+++ src/IdentityService/HostingExtensions.cs
@@ -65,6 +65,11 @@ internal static class HostingExtensions
                 options.Events.RaiseInformationEvents = true;
                 options.Events.RaiseFailureEvents = true;
                 options.Events.RaiseSuccessEvents = true;
+
+                if(builder.Environment.IsEnvironment("Docker"))
+                {
+                    options.IssuerUri= "http://localhost:5001";
+                }
                  
 
                 // Use a large chunk size for diagnostic data in development where it will be redirected to a local file.
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -8,14 +8,21 @@ using SearchService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
-builder.Services.AddAutoMapper(cfg => {}, typeof(Program));
+builder.Services.AddAutoMapper(cfg => { }, typeof(Program));
 builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetRetryPolicy());
 builder.Services.AddMassTransit(x =>
 {
     x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
-    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search",false));
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));
     x.UsingRabbitMq((context, cfg) =>
     {
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", h =>
+       {
+
+           h.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+           h.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+       });
+
         cfg.ReceiveEndpoint("search-auction-created", e =>
         {
             e.UseMessageRetry(r => r.Interval(5, TimeSpan.FromSeconds(5)));
