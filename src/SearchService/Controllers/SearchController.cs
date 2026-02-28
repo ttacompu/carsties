75ac819c508b172cc440ef9901468cfacaa086fc
ttacompu <ttacompu@gmail.com>
2026-02-28 11:47:51 -0500

    client side config is good now

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -24,7 +24,7 @@ namespace SearchService.Controllers
 
             query = searchParams.OrderBy switch
             {
-                "make" => query.Sort(x => x.Ascending(x => x.Make)),
+                "make" => query.Sort(x => x.Ascending(x => x.Make)).Sort(x => x.Ascending(a =>a.Model )),
                 "new" => query.Sort(x => x.Descending(x => x.CreatedAt)),
                 _ => query.Sort(x => x.Ascending(x => x.AuctionEnd))
             };
