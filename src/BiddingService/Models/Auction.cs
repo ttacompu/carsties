803d514c9002d09ab3babcab6dc1727b5dc0d392
ttacompu <ttacompu@gmail.com>
2026-03-03 20:25:23 -0500

    added Inital bid service

---
+++ src/BiddingService/Models/Auction.cs
@@ -0,0 +1,14 @@
+using System;
+using MongoDB.Entities;
+
+namespace BiddingService.Models;
+
+public class Auction : Entity
+{
+    public DateTime AuctionEnd{ get; set; }
+
+    public string Seller {get;set;} = string.Empty;
+    public int ReservePrice{ get; set; }
+
+    public bool Finished{ get; set; }
+}
