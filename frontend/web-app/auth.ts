ed52b61f90f348ee209ef229322e5974eb8db0c0
ttacompu <ttacompu@gmail.com>
2026-03-22 16:06:18 -0400

    switch dev and local prod

--- frontend/web-app/auth.ts
+++ frontend/web-app/auth.ts
@@ -2,42 +2,57 @@ import NextAuth, { Profile } from "next-auth"
 import { OIDCConfig } from "next-auth/providers"
 import DuendeIDS6Provider from "next-auth/providers/duende-identity-server6"
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [DuendeIDS6Provider({
+const getDuendeProvider = () => {
+  const baseConfig = {
     id: 'id-server',
     clientId: "nextApp",
     clientSecret: "secret",
-    issuer:process.env.ID_URL, 
+    issuer: process.env.ID_URL,
     authorization: {
-      params: { scope: 'openid profile auctionApp' },
-      url : process.env.ID_URL + '/connect/authorize'
-    },
-    token : {
-      url : `${process.env.ID_URL_INTERNAL}/connect/token`
-    },
-    userinfo : {
-      url : `${process.env.ID_URL_INTERNAL}/connect/token`
+      params: { scope: 'openid profile auctionApp' }
     },
     idToken: true
-  } as OIDCConfig<Omit<Profile, 'username'>>)],
-  callbacks: {
-    async redirect({url, baseUrl}){
-        return url.startsWith(baseUrl) ? url : baseUrl
+  };
+
+  const duendeConfig = (process.env.ID_URL_INTERNAL ? {
+    ...baseConfig,
+    authorization: {
+      params: { scope: 'openid profile auctionApp' },
+      url: process.env.ID_URL + '/connect/authorize'
     },
-    async authorized({auth}){
-        return !!auth;
+    token: {
+      url: `${process.env.ID_URL_INTERNAL}/connect/token`
+    },
+    userinfo: {
+      url: `${process.env.ID_URL_INTERNAL}/connect/token`
+    }
+  }
+    :
+    baseConfig) as OIDCConfig<Omit<Profile, 'username'>>;
+
+  return [DuendeIDS6Provider(duendeConfig)]
+}
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: getDuendeProvider(),
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      return url.startsWith(baseUrl) ? url : baseUrl
+    },
+    async authorized({ auth }) {
+      return !!auth;
     },
     async jwt({ token, profile, account }) {
-    if(account && account.access_token){
-      token.accessToken  = account.access_token;
-    } 
+      if (account && account.access_token) {
+        token.accessToken = account.access_token;
+      }
       if (profile) {
         token.username = profile.username;
       }
       return token;
     },
-    async session({session, token}){
-      if(token){
+    async session({ session, token }) {
+      if (token) {
         session.user.username = token.username;
         session.accessToken = token.accessToken;
       }
